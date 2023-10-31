ba124f7d6563399c19b29489120a9865fdc28479
Pedro Henrique Diedrich <pedrodiedrich123@gmail.com>
2023-10-30 22:48:10 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import Navbar from "./components/Navbar";
 import Container from "./components/Container";
 import Footer from "./components/Footer";
-import ReactGA from "react-ga4";
 
 function App() {
-  const TRACKING_ID = "G-8TXS2CK7NG";
-  ReactGA.initialize(TRACKING_ID);
-  ReactGA.pageView(document.location.pathname);
   return (
     <div className="flex flex-col">
       {/* <Navbar /> */}
