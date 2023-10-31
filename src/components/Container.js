2eaa63bdecadffd77e219d9fce293f11d7d89caa
Pedro Henrique Diedrich <pedrodiedrich123@gmail.com>
2023-10-31 18:48:15 -0300

    Update Container.js

--- src/components/Container.js
+++ src/components/Container.js
@@ -6,7 +6,10 @@ import CardOds from "./CardOds";
 
 function Container() {
   return (
-    <div className="h-full p-8 flex flex-col gap-y-6 bg-no-repeat bg-cover bg-fixed" style={{backgroundImage: 'url(https://i.imgur.com/wXsxreG.png)'}}>
+    <div
+      className="h-full p-8 flex flex-col gap-y-6 bg-no-repeat bg-cover bg-fixed"
+      style={{ backgroundImage: "url(https://i.imgur.com/wXsxreG.png)" }}
+    >
       <main className="flex flex-col gap-y-6">
         <Titulo titulo="Objetivos de Desenvolvimento Sustentável" />
         <Paragrafo
@@ -29,9 +32,11 @@ function Container() {
         titulo="Erradicação da pobreza"
         urlImg="https://i.imgur.com/6ErjJKo.png"
         alt="Erradicação"
-        msg="Reduzir pelo menos à metade a proporção de homens,
-         mulheres e crianças, de todas as idades, que vivem na pobreza, 
-         em todas as suas dimensões, de acordo com as definições nacionais."
+        msg="O projeto de reciclagem de resíduos eletrônicos pode contribuir
+          para a erradicação da pobreza, proporcionando oportunidades de
+          emprego em diferentes etapas do processo de reciclagem. Além disso,
+          a revenda de peças e componentes eletrônicos recuperados pode gerar
+          renda para pessoas que vivem em condições de pobreza."
       />
       <CardOds
         flexDirection="flex-row-reverse"
@@ -40,8 +45,12 @@ function Container() {
         titulo="Educação de Qualidade"
         urlImg="https://i.imgur.com/uRpUhIC.png"
         alt="Educação"
-        msg="Garantir o acesso à educação inclusiva, de qualidade e equitativa,
-         e promover oportunidades de aprendizagem ao longo da vida para todos"
+        msg="O projeto pode desempenhar um papel fundamental
+          na educação da população sobre a importância da reciclagem
+          de resíduos eletrônicos. Por meio do site e de programas de conscientização,
+          as pessoas podem aprender sobre o que é o e-lixo, como reciclá-lo de maneira
+          adequada e quais são os benefícios ambientais e sociais associados a essa prática.
+          Isso promove a educação de qualidade e a conscientização ambiental."
       />
       <CardOds
         background="bg-[#A21942]"
@@ -49,8 +58,11 @@ function Container() {
         titulo="Trabalho decente e crescimente econômico"
         urlImg="https://i.imgur.com/HiUdx0d.png"
         alt="Trabalho"
-        msg="Promover o crescimento econômico inclusivo e sustentável, 
-        o emprego pleno e produtivo e o trabalho digno para todos"
+        msg="Ao criar empregos na coleta, desmontagem,
+          reciclagem e recondicionamento de resíduos eletrônicos,
+          o projeto contribui para a promoção de trabalho decente 
+          e o crescimento econômico. Além disso, ao recuperar materiais
+          valiosos dos resíduos eletrônicos, o projeto pode gerar receita, impulsionando a economia local."
       />
       <CardOds
         flexDirection="flex-row-reverse"
@@ -59,8 +71,11 @@ function Container() {
         titulo="Indústria, inovação e infraestrutura"
         urlImg="https://i.imgur.com/N7fLdBG.png"
         alt="Educação"
-        msg="Construir infraestruturas resilientes,
-         promover a industrialização inclusiva e sustentável e fomentar a inovação"
+        msg="O projeto promove a inovação na gestão de resíduos
+          eletrônicos, incentivando a criação de soluções tecnológicas
+          para o tratamento e a reciclagem desses materiais. Também 
+          estimula a infraestrutura necessária para a coleta, o processamento
+          e o recondicionamento dos resíduos eletrônicos."
       />
       <CardOds
         background="bg-[#FD9D24]"
@@ -68,7 +83,12 @@ function Container() {
         titulo="Cidades e comunidades sustentáveis"
         urlImg="https://i.imgur.com/PP2gIyi.png"
         alt="Cidades"
-        msg="Tornar as cidades e comunidades mais inclusivas, seguras, resilientes e sustentáveis"
+        msg="A reciclagem de resíduos eletrônicos contribui
+          para a criação de cidades e comunidades sustentáveis,
+          reduzindo a quantidade de resíduos eletrônicos descartados
+          em aterros sanitários. Isso diminui os impactos ambientais
+          e melhora a qualidade de vida nas comunidades locais, reduzindo
+          a exposição a substâncias tóxicas presentes em eletrônicos obsoletos."
       />
       <CardOds
         flexDirection="flex-row-reverse"
@@ -77,7 +97,11 @@ function Container() {
         titulo="Consumo e produção responsáveis"
         urlImg="https://i.imgur.com/CBrOkCg.png"
         alt="Consumo"
-        msg="Garantir padrões de consumo e de produção sustentáveis"
+        msg="Promover a reciclagem de resíduos eletrônicos incentiva
+          o consumo e a produção responsáveis. As pessoas são incentivadas
+          a prolongar a vida útil de seus dispositivos eletrônicos, reutilizando
+          e reciclando-os, em vez de descartá-los prematuramente. Isso reduz a
+          demanda por novos produtos eletrônicos e minimiza o desperdício."
       />
       <CardOds
         background="bg-[#19486A]"
@@ -85,7 +109,12 @@ function Container() {
         titulo="Parcerias e meios de implementação"
         urlImg="https://i.imgur.com/yujWc5c.png"
         alt="Parcerias"
-        msg="Reforçar os meios de implementação e revitalizar a parceria global para o desenvolvimento sustentável"
+        msg="O projeto de reciclagem de resíduos eletrônicos
+          pode estabelecer parcerias com organizações governamentais,
+          empresas privadas, organizações não governamentais e comunidades
+          locais. Essas parcerias podem viabilizar a implementação bem-sucedida 
+          do projeto, reunindo recursos, conhecimento e esforços para alcançar os
+          objetivos dos ODS relacionados."
       />
     </div>
   );
