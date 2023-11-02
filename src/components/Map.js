f32f98c6b019ebc2a42b2aa2229e0173e87b7f4f
Pedro Henrique Diedrich <pedrodiedrich123@gmail.com>
2023-11-01 22:34:51 -0300

    Update Map.js

--- src/components/Map.js
+++ src/components/Map.js
@@ -25,7 +25,7 @@ function Map() {
 
   const [selectedMarker, setSelectedMarker] = useState(null);
   const image = "https://i.imgur.com/CHfXffr.jpg";
-  const icon = "https://i.imgur.com/MCsipDh.png";
+  const icon = "https://i.imgur.com/JI6JcKE.png";
   return (
     <div className="h-full w-full">
       {isLoaded && (
